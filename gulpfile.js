15d40a5a8ed514cae7cbb6490388f1626c647f25
Okpainmo <okpainmoandrew@gmail.com>
2021-07-04 18:17:44 +0100

    I made some more changes

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// variables
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("gulp-autoprefixer");
+
+// custom-hello task
+gulp.task("hello", function (done) {
+  console.log("hello from gulp");
+  done();
+});
+
+// sass task
+function buildStyles() {
+  return gulp
+    .src("Sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest("Dist/Styles"));
+}
+
+exports.buildStyles = buildStyles;
+
+// // autoprefixer task
+// exports.default = () =>
+//   gulp
+//     .src("Sass/**/*.scss")
+//     .pipe(
+//       autoprefixer({
+//         cascade: false,
+//       })
+//     )
+//     .pipe(gulp.dest("Dist/Styles/**/*.css"));
+
+// watch task
+gulp.task("watch", function () {
+  gulp.watch("Sass/**/*.scss", gulp.series("buildStyles"));
+});
