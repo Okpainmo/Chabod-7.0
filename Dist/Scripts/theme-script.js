388ecac6db8d5f75d0e085a9d148f3769d94cb62
Okpainmo <okpainmoandrew@gmail.com>
2021-07-06 10:05:54 +0100

    still fixing particles.js

--- Dist/Scripts/theme-script.js
+++ Dist/Scripts/theme-script.js
@@ -2,7 +2,7 @@ function particles() {
   alert("Page is loaded");
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
   particlesJS.load(
-    "../Scripts../Dist/particles.js-master/particles-js",
+    "particlesJs",
     "../Scripts../Dist/particles.js-master/particles-config.json",
     function () {
       console.log("callback - particles.js config loaded");
