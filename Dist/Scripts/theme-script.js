3e184be679eaddb792aa5c3a1d400bab44e78a09
Okpainmo <okpainmoandrew@gmail.com>
2021-07-19 21:48:48 +0100

    more progress

--- Dist/Scripts/theme-script.js
+++ Dist/Scripts/theme-script.js
@@ -1,25 +1,50 @@
-// function alertMe() {
-//   alert("page loaded");
-// }
-
-// Portfolio filter script
-
 $(document).ready(
+  // porfolio projects filter
+
+  $("#showAll").click(function () {
+    $(".dev").fadeIn(1000);
+    $(".UiUx").fadeIn(1000);
+    $(".graphics").fadeIn(1000);
+  }),
   $("#gSort").click(function () {
     $(".dev").fadeOut("slow");
-    $(".UiUx").hide();
-    $(".graphics").show();
+    $(".UiUx").fadeOut("slow");
+    $(".graphics").hide();
+    $(".dev").css("display", "none");
+    $(".UiUx").css("display", "none");
+    $(".graphics").fadeIn(1000);
+  }),
+  $("#devSort").click(function () {
+    $(".graphics").fadeOut("slow");
+    $(".UiUx").fadeOut("slow");
+    $(".dev").fadeOut("slow");
+    $(".UiUx").css("display", "none");
+    $(".graphics").css("display", "none");
+    $(".dev").fadeIn(1000);
+  }),
+  $("#interfaceSort").click(function () {
+    $(".graphics").fadeOut("slow");
+    $(".UiUx").fadeOut("slow");
+    $(".dev").fadeOut("slow");
+    $(".dev").css("display", "none");
+    $(".graphics").css("display", "none");
+    $(".UiUx").fadeIn(1000);
+  }),
+
+  // projects details slide in.
+  $(".read-on-menu-bar").click(function () {
+    $(this)
+      .parent()
+      .siblings()
+      .children(".slide-up-overlay")
+      .addClass("slide-up-overlay_slideUp");
+  }),
+
+  // close portfolio slide-up-overlay
+  $(".close-overlay").click(function () {
+    $(this)
+      .parent()
+      .parent(".slide-up-overlay")
+      .removeClass("slide-up-overlay_slideUp");
   })
-
-  // $("#devSort").click(function () {
-  //   $(".graphics").hide();
-  //   $("#gSort").fadeOut();
-  //   $(".dev").css("display", "block");
-  // }),
-
-  // $("#interfaceSort").click(function () {
-  //   $(".graphics").css("display", "none");
-  //   $(".dev").css("display", "none");
-  //   $(".UiUx").css("display", "block");
-  // })
 );
