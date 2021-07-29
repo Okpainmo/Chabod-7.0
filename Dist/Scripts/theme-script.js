107e018f70eac53058acff00e4e6418848403111
Okpainmo <okpainmoandrew@gmail.com>
2021-07-29 20:27:00 +0100

    making more progress

--- Dist/Scripts/theme-script.js
+++ Dist/Scripts/theme-script.js
@@ -57,56 +57,74 @@ function showUiUx() {
   });
 }
 
-// projects overlay-slide-up
+// projects overlay-slide-up and overlay-slide-down -- start
 
 const projectOverlayBtn = document.querySelectorAll(".read-on-menu-bar");
 const closeProjectOverlayBtn = document.querySelectorAll(".close-overlay");
 
-function slideUpOverlay() {
-  this.parentNode.previousSibling.previousSibling.childNodes[1].classList.remove(
-    "slide-up-overlay_slideDown"
-  );
-  this.parentNode.previousSibling.previousSibling.childNodes[1].classList.add(
-    "slide-up-overlay_slideUp"
-  );
-}
-
-function slideDownOverlay() {
-  this.parentNode.parentNode.classList.remove("slide-up-overlay_slideUp");
-  this.parentNode.parentNode.classList.add("slide-up-overlay_slideDown");
-}
-
-// Display main-overlay and main-overlay-content-container
-
-const showProjectStory = document.querySelectorAll(".project-story-link");
-
-function ShowMainOverlay() {
-  document.querySelector(".main-overlay").style.display = "block";
-  document.querySelector(".side-nav").style.filter = "blur(10px)";
-  document.querySelector(".side-content").style.filter = "blur(10px)";
-}
-
-const closeMainOverlayBtn = document.querySelectorAll(".close-main-overlay");
-
-function closeMainOverlay() {
-  document.querySelector(".main-overlay").style.display = "none";
-  document.querySelector(".side-nav").style.filter = "none";
-  document.querySelector(".side-content").style.filter = "none";
-}
-// Event Listeners
-closeMainOverlayBtn.forEach(function (each) {
-  each.addEventListener("click", closeMainOverlay);
+projectOverlayBtn.forEach(function (each) {
+  each.addEventListener("click", function () {
+    this.parentNode.previousSibling.previousSibling.childNodes[1].classList.remove(
+      "slide-up-overlay_slideDown"
+    );
+    this.parentNode.previousSibling.previousSibling.childNodes[1].classList.add(
+      "slide-up-overlay_slideUp"
+    );
+  });
 });
+
+closeProjectOverlayBtn.forEach(function (each) {
+  each.addEventListener("click", function slideDownOverlay() {
+    this.parentNode.parentNode.classList.remove("slide-up-overlay_slideUp");
+    this.parentNode.parentNode.classList.add("slide-up-overlay_slideDown");
+  });
+});
+
+// projects overlay-slide-up and overlay-slide-down -- end
+
+// Display and hide main-overlay and content(project stories and blog stories) -- start
+
+const closeMainOverlayBtn = document.querySelectorAll(
+  ".close-main-overlay-btn"
+);
+const showProjectStory = document.querySelectorAll(".project-story-link");
+const projectStories = document.querySelectorAll(".project-story");
+const showBlogStory = document.querySelectorAll(".seller-blog-story");
+const blogStories = document.querySelectorAll(".blog-story");
+
+for (let x = 0; x < 6; x++) {
+  showProjectStory[x].addEventListener("click", function () {
+    projectStories[x].style.display = "block";
+    document.querySelector(".side-nav").style.filter = "blur(10px)";
+    document.querySelector(".side-content").style.filter = "blur(10px)";
+  });
+
+  showBlogStory[x].addEventListener("click", function () {
+    blogStories[x].style.display = "block";
+    document.querySelector(".side-nav").style.filter = "blur(10px)";
+    document.querySelector(".side-content").style.filter = "blur(10px)";
+  });
+}
+
+closeMainOverlayBtn.forEach(function (each) {
+  each.addEventListener("click", function closeMainOverlay() {
+    document.querySelector(".side-nav").style.filter = "none";
+    document.querySelector(".side-content").style.filter = "none";
+    document.querySelectorAll(".main-overlay").forEach(function (each) {
+      each.style.display = "none";
+    });
+  });
+});
+
+// Display and hide main-overlay and content(project stories and blog stories) -- end
+
+// Event Listeners
+
 gSort.addEventListener("click", showGraphics);
 devSort.addEventListener("click", showDev);
 UiUxSort.addEventListener("click", showUiUx);
 showAll.addEventListener("click", displayAll);
-projectOverlayBtn.forEach(function (each) {
-  each.addEventListener("click", slideUpOverlay);
-});
-closeProjectOverlayBtn.forEach(function (each) {
-  each.addEventListener("click", slideDownOverlay);
-});
-showProjectStory.forEach(function (each) {
-  each.addEventListener("click", ShowMainOverlay);
-});
+
+// showProjectStory.forEach(function (each) {
+//   each.addEventListener("click", ShowMainOverlay);
+// });
